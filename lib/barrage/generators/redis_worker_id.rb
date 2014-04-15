acdb685199cba31b097bed67176a9e15660d31c5
gussan <egussan@gmail.com>
2014-04-15 22:15:22 +0900

    Free worker_id on finalizer

--- lib/barrage/generators/redis_worker_id.rb
+++ lib/barrage/generators/redis_worker_id.rb
@@ -10,16 +10,46 @@ class Barrage
         @worker_id = nil
         @worker_ttl = 0
         super
+        @data = []
+        @finalizer_proc = Finalizer.new(@data)
+        ObjectSpace.define_finalizer(self, @finalizer_proc)
       end
 
       def generate
         now = Time.now.to_i
         if @worker_ttl - now <= 0
-          @worker_id = renew_worker_id
+          @data[1] = @worker_id = renew_worker_id
           @worker_ttl = now + ttl / 2 # 1/2 以上過ぎたら再度取得
         end
         @worker_id
       end
+
+      def redis
+        @redis ||= @data[0] = Redis.new(options["redis"] || {})
+      end
+
+      def ttl
+        options["ttl"]
+      end
+
+      class Finalizer
+        def initialize(data)
+          @pid = $$
+          @data = data
+        end
+
+        def call(*args)
+          return if @pid != $$
+          redis, worker_id = *@data
+
+          if redis.is_a?(Redis) and redis.connected?
+            redis.del("barrage:worker:#{worker_id}")
+            redis.client.disconnect
+          end
+        end
+      end
+
+      private
 
       def renew_worker_id
         redis.evalsha(
@@ -28,15 +58,9 @@ class Barrage
         ).to_i
       end
 
-      def redis
-        @redis = Redis.new(options["redis"] || {})
+      def return_worker_id
+        redis.del("barrage:worker:#{@worker_id}")
       end
-
-      def ttl
-        options["ttl"]
-      end
-
-      private
 
       def script_sha
         @script_sha ||=
