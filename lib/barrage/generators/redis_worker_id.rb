554373fb1aa01f039e784fac2f5ee3b0a00298b5
gussan <egussan@gmail.com>
2014-06-26 15:54:50 +0900

    Fix RedisWorkerId
    * give up when trying loops count reached max_value
    * raise error when could not to get worker_id

--- lib/barrage/generators/redis_worker_id.rb
+++ lib/barrage/generators/redis_worker_id.rb
@@ -64,30 +64,41 @@ class Barrage
         new_worker_id = redis.evalsha(
           script_sha,
           argv: [2 ** length, rand(2 ** length), @worker_id, ttl, RACE_CONDITION_TTL]
-        ).to_i
-        new_worker_id
+        )
+        new_worker_id or raise StandardError, "Renew redis worker id failed"
+        return new_worker_id.to_i
       end
 
       def script_sha
         @script_sha ||=
           redis.script(:load, <<-EOF.gsub(/^ {12}/, ''))
-            local max_value = ARGV[1]
+            local max_value = tonumber(ARGV[1])
             local new_worker_id = ARGV[2]
             local old_worker_id = ARGV[3]
-            local ttl = ARGV[4]
-            local race_condition_ttl = ARGV[5]
+            local ttl = tonumber(ARGV[4])
+            local race_condition_ttl = tonumber(ARGV[5])
+            local loop_cnt = 0
+
+            local worker_id = nil
+            local candidate_worker_id = tonumber(new_worker_id)
 
             if type(old_worker_id) == "string" and string.len(old_worker_id) > 0 and redis.call('EXISTS', "barrage:worker:" .. old_worker_id) == 1 then
               redis.call("EXPIRE", "barrage:worker:" .. old_worker_id, ttl + race_condition_ttl)
-              new_worker_id = old_worker_id
+              worker_id = old_worker_id
             else
-              while redis.call("SETNX", "barrage:worker:" .. new_worker_id, 1) == 0
+              while redis.call("SETNX", "barrage:worker:" .. candidate_worker_id, 1) == 0 and loop_cnt < max_value
               do
-                new_worker_id = (new_worker_id + 1) % max_value
+                candidate_worker_id = (candidate_worker_id + 1) % max_value
+                loop_cnt = loop_cnt + 1
               end
-              redis.call("EXPIRE", "barrage:worker:" .. new_worker_id, ttl + race_condition_ttl)
+              if loop_cnt >= max_value then
+                return nil
+              else
+                worker_id = candidate_worker_id
+              end
+              redis.call("EXPIRE", "barrage:worker:" .. worker_id, ttl + race_condition_ttl)
             end
-            return new_worker_id
+            return worker_id
           EOF
       end
     end
