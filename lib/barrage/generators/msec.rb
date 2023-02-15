206759fc243e5acc705a5bf5af9be8cbf69eb984
hiroi_junki <junki.hiroi@drecom.co.jp>
2023-02-15 13:42:22 +0900

    Update Test msec を 5分に1増加するように対応

--- lib/barrage/generators/msec.rb
+++ lib/barrage/generators/msec.rb
@@ -6,7 +6,7 @@ class Barrage
       self.required_options += %w(start_at)
 
       def generate
-        ((Time.now.to_f * 1000).round - start_at) & (2 ** length - 1)
+        (Time.now.to_i / 60 / 5) & (2 ** length - 1)
       end
 
       alias_method :current, :generate
